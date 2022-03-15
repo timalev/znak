f2a23c61408da1a1dd4e983b7cb9921e1f81d377
Petrlev <levendeevp@gmail.com>
2022-03-15 19:55:50 +0300

    teh

--- app/src/androidTest/java/io/cordova/test2_6720b/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/io/cordova/test2_6720b/ExampleInstrumentedTest.java
@@ -1,8 +1,8 @@
 package io.cordova.test2_6720b;
 
 import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
--- tmp/ProfileTheme/app/src/main/java/com/example/kamran/profiletheme/MainActivity.java
+++ tmp/ProfileTheme/app/src/main/java/com/example/kamran/profiletheme/MainActivity.java
@@ -1,6 +1,6 @@
 package com.example.kamran.profiletheme;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity
